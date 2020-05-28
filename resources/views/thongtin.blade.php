456f683802ed4b2b9b4b796947c297f93282c9de
Your Name <you@example.com>
2020-05-28 21:33:58 +0700

    giao dien hien thi thong tin ho so

--- resources/views/thongtin.blade.php
+++ resources/views/thongtin.blade.php
@@ -21,144 +21,154 @@
           <thead>
             <tr>
               <th scope="row">Họ Tên:</th>
-              <td>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.</td>
+              <td>Nguyễn Văn A</td>
+              <th scope="row">Giới Tính:</th>
+              <td>Mark</td>
+              <th scope="row">Ngày Tháng Năm Sinh:</th>
+              <td>Jacob</td>
+              <th scope="row">Nơi Sinh:</th>
+              <td>Larry</td>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <th scope="row">Giới Tính:</th>
-              <td>Mark</td>
-            </tr>
-            <tr>
-              <th scope="row">Ngày Tháng Năm Sinh:</th>
-              <td>Jacob</td>
-            </tr>
-            <tr>
-              <th scope="row">Nơi Sinh:</th>
-              <td>Larry</td>
-            </tr>
-            <tr>
               <th scope="row">Dân Tộc:</th>
               <td>Mark</td>
-            </tr>
-            <tr>
               <th scope="row">CMND:</th>
               <td>Jacob</td>
-            </tr>
-            <tr>
               <th scope="row">Ngày Cấp:</th>
               <td>Larry</td>
-            </tr>
-            <tr>
               <th scope="row">Nơi Cấp:</th>
               <td>Mark</td>
             </tr>
             <tr>
               <th scope="row">Hộ Khẩu:</th>
               <td>Jacob</td>
-            </tr>
-            <tr>
               <th scope="row">Mã Tỉnh:</th>
               <td>Larry</td>
-            </tr>
-            <tr>
               <th scope="row">Mã Huyện:</th>
               <td>Mark</td>
-            </tr>
-            <tr>
               <th scope="row">Mã Xã:</th>
               <td>Jacob</td>
-            </tr>
+           </tr>
             <tr>
               <th scope="row">SĐT:</th>
               <td>Larry</td>
-            </tr>
-            <tr>
               <th scope="row">Email:</th>
               <td>Mark</td>
-            </tr>
-            <tr>
               <th scope="row">Địa Chỉ:</th>
               <td>Jacob</td>
-            </tr>
-            <tr>
               <th scope="row">Năm Tốt Nghiệp:</th>
               <td>Larry</td>
             </tr>
             <tr>
               <th scope="row">KV Ưu Tiên:</th>
               <td>Mark</td>
-            </tr>
-            <tr>
               <th scope="row">Đối Tượng Ưu Tiên:</th>
               <td>Jacob</td>
-            </tr>
-            <tr>
               <th scope="row">Ảnh Học Bạ:</th>
+              <td colspan="2">Larry the Bird aaaaaaaaaaaaaaaaaaa</td>
+            </tr>
+            <tr>
+              <th scope="row">Lớp 10</th>
+            </tr>
+            <tr>
+              <th scope="row">Tên Trường:</th>
+              <td>Larry</td>
+              <th scope="row">Địa Chỉ:</th>
+              <td>Larry</td>
+              <th scope="row">Mã Trường:</th>
               <td>Larry</td>
             </tr>
             <tr>
-              <th scope="row">Trạng Thái:</th>
+              <th scope="row">Điểm TB Môn 1:</th>
+              <td>Larry</td>
+              <th scope="row">Điểm TB Môn 2:</th>
+              <td>Larry</td>
+              <th scope="row">Điểm TB Môn 3:</th>
               <td>Larry</td>
             </tr>
             <tr>
-              <th scope="row">3</th>
+              <th scope="row">Lớp 11</th>
+            </tr>
+            <tr>
+              <th scope="row">Tên Trường:</th>
+              <td>Larry</td>
+              <th scope="row">Địa Chỉ:</th>
+              <td>Larry</td>
+              <th scope="row">Mã Trường:</th>
               <td>Larry</td>
             </tr>
             <tr>
-              <th scope="row">3</th>
+              <th scope="row">Điểm TB Môn 1:</th>
+              <td>Larry</td>
+              <th scope="row">Điểm TB Môn 2:</th>
+              <td>Larry</td>
+              <th scope="row">Điểm TB Môn 3:</th>
               <td>Larry</td>
             </tr>
             <tr>
-              <th scope="row">3</th>
+              <th scope="row">Lớp 12</th>
+            </tr>
+            <tr>
+              <th scope="row">Tên Trường:</th>
+              <td>Larry</td>
+              <th scope="row">Địa Chỉ:</th>
+              <td>Larry</td>
+              <th scope="row">Mã Trường:</th>
               <td>Larry</td>
             </tr>
             <tr>
-              <th scope="row">3</th>
+              <th scope="row">Điểm TB Môn 1:</th>
+              <td>Larry</td>
+              <th scope="row">Điểm TB Môn 2:</th>
+              <td>Larry</td>
+              <th scope="row">Điểm TB Môn 3:</th>
               <td>Larry</td>
             </tr>
             <tr>
-              <th scope="row">3</th>
+              <th scope="row">Nguyện Vọng 1</th>
+            </tr>
+            <tr>
+              <th scope="row">Tên Ngành:</th>
+              <td>Larry</td>
+              <th scope="row">Mã Ngành:</th>
+              <td>Larry</td>
+              <th scope="row">Tổ Hợp:</th>
               <td>Larry</td>
             </tr>
             <tr>
-              <th scope="row">3</th>
+              <th scope="row">Nguyện Vọng 2</th>
+            </tr>
+            <tr>
+              <th scope="row">Tên Ngành:</th>
+              <td>Larry</td>
+              <th scope="row">Mã Ngành:</th>
+              <td>Larry</td>
+              <th scope="row">Tổ Hợp:</th>
               <td>Larry</td>
             </tr>
             <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
+              <th scope="row">Nguyện Vọng 3</th>
             </tr>
             <tr>
-              <th scope="row">3</th>
+              <th scope="row">Tên Ngành:</th>
               <td>Larry</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
+              <th scope="row">Mã Ngành:</th>
+              <td>Larry</td>
+              <th scope="row">Tổ Hợp:</th>
               <td>Larry</td>
             </tr>
           </tbody>
         </table>
-    id: 1
-    Tên :
-    Giới Tính:
-    Ngày Tháng Năm Sinh:
-    Nơi Sinh :
-    Dan Tộc   :         
-    CMND       :      
-    Ngày Cấp    :        
-    Nơi Cấp      :      
-    Hộ Khẩu       :      
-    Mã Tỉnh        :    
-    Mã Huyện        :    
-    Mã Xã            : 
-    SĐT              :
-    Emai              :
-    Địa Chỉ            :
-    Năm Tốt Nghiệp     :
-    Kh Vực Ưu Tiên     :
-    Đối Tượng Ưu Tiên   :
-    Trạng Thái   :  
+        <div class="col-md-12">Trạng thái hồ sơ:</div>
+        <div class="alert alert-success">
+          <strong>Đã Duyệt</strong>
+        </div>
+        <div class="alert alert-danger">
+          <strong>Đang Chờ Duyệt</strong>
+        </div>
+        <div class="btn btn-success">Về Trang Chủ>></div>
     </div>
 </body>
 </html>
